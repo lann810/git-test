537c1e892d18fc3021f50d8797a0455da4e21374
lanbiwen <lbw010810@163.com>
2023-03-29 22:05:42 +0800

    first commit

---
+++ src/main/java/com/lan/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.lan.git;
+
+/**
+ * @author LanBiWen
+ * @version 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}

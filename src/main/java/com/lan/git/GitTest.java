64e578fbfce42f7e0a43914d85a87fa3ae90caf6
lann <lbw010810@163.com>
2023-03-29 23:50:32 +0800

    push test

--- src/main/java/com/lan/git/GitTest.java
+++ src/main/java/com/lan/git/GitTest.java
@@ -12,5 +12,6 @@ public class GitTest {
         System.out.println("hot-fix test");
         System.out.println("master test");
         System.out.println("hot-fix test2");
+        System.out.println("push test");
     }
 }

37d9122a8c78e29048823cc1ade37bfe43ba9e8b
lanbiwen <lbw010810@163.com>
2023-03-29 22:09:54 +0800

    third commit

--- src/main/java/com/lan/git/GitTest.java
+++ src/main/java/com/lan/git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hello git2");
+        System.out.println("hello git3");
     }
 }

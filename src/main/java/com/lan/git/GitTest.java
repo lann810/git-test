1b4ba6f94c1bb9f0936e215821c378ae83b8f094
lanbiwen <lbw010810@163.com>
2023-03-29 22:08:07 +0800

    second commit

--- src/main/java/com/lan/git/GitTest.java
+++ src/main/java/com/lan/git/GitTest.java
@@ -7,5 +7,6 @@ package com.lan.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git2");
     }
 }

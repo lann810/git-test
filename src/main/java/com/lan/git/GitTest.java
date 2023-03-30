b666701856578d88e803dd19da6b75a3a9c657b6
lann <102134783+it-lan@users.noreply.github.com>
2023-03-30 21:25:44 +0800

    Update GitTest.java

--- src/main/java/com/lan/git/GitTest.java
+++ src/main/java/com/lan/git/GitTest.java
@@ -13,5 +13,6 @@ public class GitTest {
         System.out.println("master test");
         System.out.println("hot-fix test2");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
